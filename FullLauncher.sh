47a7fb91002d6ea053a2c63ef7398dd5315ee143
Saygın Doğu <sayginzdogu@gmail.com>
2015-01-21 16:47:48 +0100

    Total word count results added, results folder structured.

--- FullLauncher.sh
+++ FullLauncher.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+hadoop fs -rm -r FullPigTable
+hadoop fs -rm -r FullShoutingWords
+
+#be careful with CRLF line endings. That will cause problems in hadoop fs path names.
 hadoop jar bigdata-0.2.jar nl.utwente.bigdata.shouting.PigTableGenerator /user/alyr/worldcup/* FullPigTable
 hadoop jar bigdata-0.2.jar nl.utwente.bigdata.shouting.ShoutingExtactor /user/alyr/worldcup/* FullShoutingWords
 hadoop jar bigdata-0.2.jar nl.utwente.bigdata.shouting.Sorter FullShoutingWords/* FullShoutingWordsSorted
